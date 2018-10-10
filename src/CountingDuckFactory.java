654c7ff6227977ac7e057472eb6be7b77c8021d8
xallo <thun.r@ku.th>
2018-10-11 02:00:53 +0700

    initial commit

---
+++ src/CountingDuckFactory.java
@@ -0,0 +1,23 @@
+public class CountingDuckFactory extends AbstractDuckFactory {
+
+
+    @Override
+    public Quackable createMallardDuck() {
+        return new QuackCounter(new MallardDuck());
+    }
+
+    @Override
+    public Quackable createRedheadDucl() {
+        return new QuackCounter(new RedHeadDuck());
+    }
+
+    @Override
+    public Quackable createDuckCall() {
+        return new QuackCounter(new DuckCall());
+    }
+
+    @Override
+    public Quackable createRubberDuck() {
+        return new QuackCounter(new RubberDuck());
+    }
+}
